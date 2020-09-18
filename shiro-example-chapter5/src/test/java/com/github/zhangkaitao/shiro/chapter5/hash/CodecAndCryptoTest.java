1302fb0b4b1eeaf503cc57a7143a67dc16e6603f
ljcyu <ljcyu@qq.com>
2020-09-18 11:31:40 +0800

    试试HashService和request在参数设置上的区别，都可以设置一些参数。

--- shiro-example-chapter5/src/test/java/com/github/zhangkaitao/shiro/chapter5/hash/CodecAndCryptoTest.java
+++ shiro-example-chapter5/src/test/java/com/github/zhangkaitao/shiro/chapter5/hash/CodecAndCryptoTest.java
@@ -125,15 +125,18 @@ public class CodecAndCryptoTest {
     @Test
     public void testHashService() {
         DefaultHashService hashService = new DefaultHashService(); //默认算法SHA-512
-        hashService.setHashAlgorithmName("SHA-512");
-        hashService.setPrivateSalt(new SimpleByteSource("123")); //私盐，默认无
-        hashService.setGeneratePublicSalt(true);//是否生成公盐，默认false
-        hashService.setRandomNumberGenerator(new SecureRandomNumberGenerator());//用于生成公盐。默认就这个
+        hashService.setHashAlgorithmName("MD5");
+        //hashService.setPrivateSalt(new SimpleByteSource("123")); //私盐，默认无
+        //hashService.setGeneratePublicSalt(true);//是否生成公盐，默认false
+        //hashService.setRandomNumberGenerator(new SecureRandomNumberGenerator());//用于生成公盐。默认就这个
         hashService.setHashIterations(1); //生成Hash值的迭代次数
 
         HashRequest request = new HashRequest.Builder()
-                .setAlgorithmName("MD5").setSource(ByteSource.Util.bytes("hello"))
-                .setSalt(ByteSource.Util.bytes("123")).setIterations(2).build();
+                //.setAlgorithmName("MD5")
+                .setSource(ByteSource.Util.bytes("hello"))
+                .setSalt(ByteSource.Util.bytes("123"))
+                //.setIterations(2)
+                .build();
         String hex = hashService.computeHash(request).toHex();
         System.out.println(hex);
     }
