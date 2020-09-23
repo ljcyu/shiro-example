ea0cd6459346cbff1b9718728c07b17bd8fd9e48
ljcyu <ljcyu@qq.com>
2020-09-23 10:42:48 +0800

    从第五章拷贝过来的jdbc中需要的entity、dao、service

---
+++ shiro-example-chapter7/src/main/java/service/PermissionServiceImpl.java
@@ -0,0 +1,23 @@
+package service;
+
+import dao.PermissionDao;
+import dao.PermissionDaoImpl;
+import entity.Permission;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 14-1-28
+ * <p>Version: 1.0
+ */
+public class PermissionServiceImpl implements PermissionService {
+
+    private PermissionDao permissionDao = new PermissionDaoImpl();
+
+    public Permission createPermission(Permission permission) {
+        return permissionDao.createPermission(permission);
+    }
+
+    public void deletePermission(Long permissionId) {
+        permissionDao.deletePermission(permissionId);
+    }
+}
